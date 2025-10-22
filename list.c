6e6beb4adcae41861b6ec6b1bd4ebd0dbb1b5422
Alexis-Maringe <alexis.maringe@efrei.net>
2025-10-22 16:16:47 +0200

    ajout_liste

--- list.c
+++ list.c
@@ -14,22 +14,12 @@ t_cell create_cell(int summit, int probability) {
     return cell;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-void ajout_cell(t_list *mylist, int summit , int probability){
-    t_cell * new = cree_cell(summit,probability);
+void add_cell(t_list *mylist, int summit , int probability){
+    t_cell * new = create_cell(summit,probability);
     new->next = mylist->head;
     mylist->head = new;
 }
+
 t_list* create_empty_list(){
     t_list* list;
 }
