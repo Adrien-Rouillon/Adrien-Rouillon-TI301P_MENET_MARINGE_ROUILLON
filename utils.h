8caab91f0b99711f1f5c6f24cb29d40558aef34d
Adrien-Rouillon <adrien.rouillon@efrei.net>
2025-11-07 11:37:48 +0100

    Merged list in utils

--- utils.h
+++ utils.h
@@ -1,7 +1,36 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
-#include "list.h"
+typedef struct s_cell {
+    int summit;
+    float probability;
+    struct s_cell *next;
+} t_cell;
+
+typedef struct s_list{
+    t_cell *head;
+} t_list;
+
+typedef struct s_adjacent_list {
+    int size;
+    t_list *list;
+} t_adjacent_list;
+
+t_cell *create_cell(int summit, float probability);
+
+t_list create_empty_list();
+
+void add_cell(t_list *mylist, int summit , float probability);
+
+void display_list(t_list *list);
+
+t_adjacent_list create_adj_list(int size);
+
+void display_adj_list(t_adjacent_list list);
+
+t_adjacent_list readGraph (const char *filename);
+
+int is_markov_graph(t_adjacent_list graph);
 
 typedef struct s_diagram {
     char * layout;
