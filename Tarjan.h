fc0be6756e454601670ace12e23cb28e90d05e6a
Alexis-Maringe <alexis.maringe@efrei.net>
2025-11-13 14:36:59 +0100

    structure tarjan

--- Tarjan.h
+++ Tarjan.h
@@ -4,7 +4,12 @@
 
 #ifndef INC_2526_TI301_PJT_TARJAN_H
 #define INC_2526_TI301_PJT_TARJAN_H
+typedef struct s_tarjan {
+  int identifiant;
+  int numero;
+  int num_acess;
+  int indic;
 
-
+}t_tarjan
 
 #endif //INC_2526_TI301_PJT_TARJAN_H
