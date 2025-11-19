3cca5d02cd6880990cad94f73240f0835b1a69e9
Adrien-Rouillon <adrien.rouillon@efrei.net>
2025-11-19 22:36:24 +0100

    Finished setting up tarjan.h

--- tarjan.c
+++ tarjan.c
@@ -6,8 +6,8 @@
 #include <stdlib.h>
 #include "utils.h"
 
-void add_tarjan(t_tarjan_list *list, int id, int num, int num_access, int indic) {
-    t_tarjan *new = malloc(sizeof(t_tarjan));
+void add_tarjan(t_tarjanVtl *list, int id, int num, int num_access, int indic) {
+    t_tarjanVtx *new = malloc(sizeof(t_tarjanVtx));
     if (id != 0)new->id = id;
     new->temp_num = num;
     new->access_num = num_access;
@@ -15,14 +15,17 @@ void add_tarjan(t_tarjan_list *list, int id, int num, int num_access, int indic)
 
 }
 
-t_tarjan_list listAdj_to_Tarjan(t_adjacent_list graph) {
-    t_tarjan_list list = malloc(sizeof(t_class));
-    if (graph.size == 0) list->size;
+t_tarjanVtl listAdj_to_Tarjan(t_adjacent_list graph) {
+    if (graph.size <= 0) perror("ERROR: listAdj_to_Tarjan");
+    t_tarjanVtx * list = malloc(sizeof(t_tarjanVtx)*(graph.size));
     for (int i = 0; i < graph.size; i++) {
-        const t_cell *cell = graph.list[i].head;
-        while (cell) {
-            t_tarjan *new = malloc(sizeof(t_tarjan));
-            cell = cell->next;
-        }
+        list[i].id = i+1;
+        list[i].temp_num = -1;
+        list[i].access_num = -1;
+        list[i].indic = 0;
     }
+    t_tarjanVtl res;
+    res.size = graph.size;
+    res.summit = list;
+    return res;
 }
--- tarjan.h
+++ tarjan.h
@@ -1,23 +1,31 @@
 #ifndef INC_2526_TI301_PJT_TARJAN_H
 #define INC_2526_TI301_PJT_TARJAN_H
-typedef struct s_tarjanV {
-  int id;
-  int temp_num;
-  int access_num;
-  int indic;
-}t_tarjan;
+typedef struct s_tarjanVtx { //Vertex
+  int id; //indentifiant du sommet dans le graphe
+  int temp_num; //ordre de découverte
+  int access_num; //regrouper les sommets qui communiquent
+  int indic; //1 si le sommet est dans la pile, 0 sinon
+}t_tarjanVtx;
 
-typedef struct s_tarjanVertices {
+typedef struct s_tarjanVtl { //Verticles
   int size;
-  t_tarjan *summit;
-} t_tarjan_vtl;
+  t_tarjanVtx *summit;
+} t_tarjanVtl;
 
 typedef struct s_class {
   char *name;
-  int *id_summit;
+  int *id_summit; //indentifiant du sommet dans la classe
   int size;
 } t_class;
 
-typedef  t_tarjan_vtl *t_tarjan_list;
+typedef struct s_partition {
+  t_class *classes;
+  int nb_classes;
+}t_partition;
+
+typedef struct s_pile {
+  int *elements;
+  int size;
+};
 
 #endif //INC_2526_TI301_PJT_TARJAN_H
