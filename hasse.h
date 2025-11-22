efe656b9f412dd40564d6a865361c0c515bdd489
Alexis-Maringe <alexis.maringe@efrei.net>
2025-11-22 11:42:24 +0100

    hasse.c et hasse.h

--- hasse.h
+++ hasse.h
@@ -12,4 +12,44 @@ void removeTransitiveLinks(t_link_array *p_link_array);
  * @return The created link array.
  */
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+typedef struct lien{
+	int start;
+    int arive;
+}lien;
+
+typedef struct {
+	lien *liens;
+	int nb_liens;
+    int capacite;
+}liens;
+typedef struct classify{
+	int sommet;
+    int classe;
+}classify;
+void initialiser_liens(liens *container);
+void redimensionner_liens(liens *container);
+int get_classe(int id_sommet, classify tableau_classif[], int nb_sommets_total);
+int lien_existe(int c_dep, int c_arr, liens *container);
+void ajouter_lien(int c_dep, int c_arr, liens *container);
+void stck_le_lien(int n, struct classify *C);
+
+int lien_existe(int c_dep, int c_arr, liens *container);
+void ajouter_lien(int c_dep, int c_arr, liens *container);
 #endif
